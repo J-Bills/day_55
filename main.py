49b6015d03d81c9aeef3282287514f74c1d94482
J-Bills <billsjamal@gmail.com>
2024-06-05 17:50:27 -0700

    applying styling using decorators

--- main.py
+++ main.py
@@ -3,14 +3,37 @@ import time
 
 app = Flask(__name__)
 
+#adding html styling to the hello_world() functin using decorators
+def make_bold(function):
+    def wrapper():
+        return f'<b>{function()}</b>'
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return f'<em>{function()}</em>'
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return f'<u>{function()}</u>'
+    return wrapper
+
+
+
 @app.route("/")
+@make_bold
+@make_emphasis
+@make_underlined
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!</p>" \
+            # '<img src="https://t3.ftcdn.net/jpg/02/95/44/22/360_F_295442295_OXsXOmLmqBUfZreTnGo9PREuAPSLQhff.jpg">' \
+            # '<h1 style="text-align: center">Picture</h1>'
 
 #Creating pre-specified data types for our paths
 @app.route("/<path:username>/<name>/<int:number>")
 def greetings(name, number, username):
-    return f"hello {name}! you are the {number} years old! \n {username}"
+    return '<h1 style=" text-align: center">f"hello {name}! you are the {number} years old! \n {username}"</h1>'
 
 # def delay_decorator(function):
 #     def wrapped():
